11c1181f60fb1b2aa688d0473072cdc553fda7b1
Ujjawal Besra <ujjawal.besra@finastra.com>
2019-07-01 11:42:06 +0530

    loading of Notebooks

--- sysinfoapp/script.js
+++ sysinfoapp/script.js
@@ -20,7 +20,6 @@ var myapp = {
         for (let i = 0; i < len; i++) {
             document.getElementById("sidenav").getElementsByTagName("a").item(i).style.visibility = "hidden";
         }
-
     },
     load: function (event) {
         var nbook = event.target.text;
@@ -28,12 +27,59 @@ var myapp = {
         fs.readFile('file.json', (err, data) => {
             if (err) throw err;
             json = JSON.parse(data.toString());
-            var node = document.createElement("a");
-            var textnode = document.createTextNode(json.Notebooks[0].Nbookname);
-            node.appendChild(textnode);
-            document.getElementById("sidenav-content").appendChild(node);
+            for (let i = 0; i < json.Notebooks.length; i++) {
+                if (json.Notebooks[i].Nbookname === nbook) {
+                    for (let j = 0; j < json.Notebooks[i].Notes.length; j++) {
+                        var node = document.createElement("a");
+                        var textnode = document.createTextNode(json.Notebooks[i].Notes[j].Notename);
+                        node.appendChild(textnode);
+                        document.getElementById("sidenav-content").appendChild(node);
+                    }
+                }
+            }
         });
 
-    }
+    },
 
+    loadNotebook : function(){
+        fs.readFile('file.json',(err, data) => {
+            if(err){
+                throw err;                
+            }
+            var json = JSON.parse(data.toString());
+            removeAllChildNodes();
+            for (let i = 0; i < json.Notebooks.length; i++) {
+                var nbookname =json.Notebooks[i].Nbookname;
+                var textNode = document.createTextNode(nbookname);
+                var node = document.createElement("a");
+                node.appendChild(textNode);
+                node.addEventListener('click',(event) => { 
+                    var nbook = event.target.text;
+                    for(let i =0 ; i < json.Notebooks.length; i++){
+                        if (json.Notebooks[i].Nbookname === nbook) {
+                            removeAllChildNodes();
+                            
+                            for (let j = 0; j < json.Notebooks[i].Notes.length; j++) {
+                                var textnode = document.createTextNode(json.Notebooks[i].Notes[j].Notename);
+                                var node = document.createElement("a");
+                                node.appendChild(textnode);                            
+                                document.getElementById("sidenav-content").appendChild(node);
+                            }
+                        }
+                    }
+                });
+                document.getElementById("sidenav-content").appendChild(node);
+            }
+        });
+    },
+    
+}
+
+function removeAllChildNodes() {
+    var navNode = document.getElementById("sidenav-content");
+    var lastchild = navNode.lastElementChild;
+    while (lastchild) {
+        navNode.removeChild(lastchild);
+        lastchild = navNode.lastElementChild;
+    }
 }
