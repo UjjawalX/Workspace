5c3055b3d67bbf9ebd4a2237eb7e37e5fc3b5238
ujjawal <besrau@yahoo.com>
2019-11-18 02:49:39 +0530

    added logic to create notes and notebooks with plus button and some css on input

--- sysinfoapp/script.js
+++ sysinfoapp/script.js
@@ -1,6 +1,8 @@
 var fs = require('fs-extra');
 var lodash = require('lodash');
 var notefilename;
+var json;
+var whichNb = -1;
 var myapp = {
     open: function () {
         document.getElementById("sidenav").style.width = "125px";
@@ -30,36 +32,50 @@ var myapp = {
             if (err) {
                 throw err;
             }
-            var json = JSON.parse(data.toString());
-            removeAllChildNodes(document.getElementById("sidenav-content"));
-            for (let i = 0; i < json.Notebooks.length; i++) {                
-                var node = createContentElement(json.Notebooks[i].Nbookname);                                
-                node.addEventListener('click', (event) => {                    
-                    removeAllChildNodes(document.getElementById("sidenav-content"));
-                    for (let j = 0; j < json.Notebooks[i].Notes.length; j++) {                        
-                        var node = createContentElement(json.Notebooks[i].Notes[j].Notename);
-                        node.addEventListener('click', (event) => {                                                     
-                            notefilename = json.Notebooks[i].Notes[j].Content;
-                            fs.readFile(notefilename , (err,data) => {                                
-                                document.getElementById("textarea").value = data;
-                            })
-                        });
-                        document.getElementById("sidenav-content").appendChild(node);
-                    }
-                });
-                document.getElementById("sidenav-content").appendChild(node);
-            }
+            json = JSON.parse(data.toString());            
+            createListNotebooks();
         });
     },
-    save : function (){
-        fs.writeFile(notefilename,document.getElementById("textarea").value,(err) => {
-            if(err)
+    save: function () {
+        fs.writeFile(notefilename, document.getElementById("textarea").value, (err) => {
+            if (err)
                 throw err;
             console.log("file saved");
         });
     },
-    createNewEntry: function(){
+    createNewEntry: function () {
         var node = document.createElement("Input");
+        document.getElementById("sidenav-content").appendChild(node);
+        node.addEventListener("blur", (ev) => {
+            var tx = ev.target.value;
+            if (whichNb != -1) {
+                var newNo = {};
+                newNo.Notename = tx;
+                newNo.Content = 'notes/'+tx;
+                json.Notebooks[whichNb].Notes[json.Notebooks[whichNb].Notes.length] = newNo; 
+                createListOfNotes(whichNb);            
+
+            } else {
+                var newNb = {};
+                newNb.Nbookname = tx;
+                newNb.Notes = [];
+                json.Notebooks[json.Notebooks.length] = newNb;               
+                createListNotebooks();    
+
+            }
+            fs.writeFile("file.json", JSON.stringify(json)); 
+            
+
+        });
+    }
+}
+
+function createListNotebooks() {
+    removeAllChildNodes(document.getElementById("sidenav-content"));
+    for (let i = 0; i < json.Notebooks.length; i++) {
+        whichNb = -1;
+        var node = createContentElement(json.Notebooks[i].Nbookname);
+        node.addEventListener('click', (event) => createListOfNotes(i));
         document.getElementById("sidenav-content").appendChild(node);
     }
 }
@@ -73,27 +89,42 @@ function removeAllChildNodes(parentNode) {
     }
 }
 
-function createContentElement(name){    
+function createContentElement(name) {
     var node;
-    if(name.length > 10){
-        shortName = name.substr(0,8).concat("..");
+    if (name.length > 10) {
+        shortName = name.substr(0, 8).concat("..");
 
-        node = createChildNode(shortName,"a");
-        node.addEventListener('mouseover',(event) => {
-            node.appendChild(createChildNode(name,"span"));
+        node = createChildNode(shortName, "a");
+        node.addEventListener('mouseover', (event) => {
+            node.appendChild(createChildNode(name, "span"));
         });
-        node.addEventListener('mouseout',(event) => {            
+        node.addEventListener('mouseout', (event) => {
             removeAllChildNodes(node);
         });
     } else {
-        node = createChildNode(name,"a");
+        node = createChildNode(name, "a");
     }
     return node;
 }
 
-function createChildNode(name,tag){
+function createChildNode(name, tag) {
     var textNode = document.createTextNode(name);
     var node = document.createElement(tag);
     node.appendChild(textNode);
     return node;
+}
+
+function createListOfNotes(i){
+    removeAllChildNodes(document.getElementById("sidenav-content"));
+    whichNb = i;
+    for (let j = 0; j < json.Notebooks[i].Notes.length; j++) {                        
+        var node = createContentElement(json.Notebooks[i].Notes[j].Notename);
+        node.addEventListener('click', (event) => {
+            notefilename = json.Notebooks[i].Notes[j].Content;
+            fs.readFile(notefilename, (err, data) => {
+                document.getElementById("textarea").value = data;
+            })
+        });
+        document.getElementById("sidenav-content").appendChild(node);
+    }
 }
